ddf7e91588082006fd0fa7b8bed765f7c773ce6c
student <student@Jesses-MacBook-Air.local>
2018-02-26 23:34:09 -0600

    Added methods to and view controllers

---
+++ TableViewApp/TableViewApp/FruitsTableViewSource.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using UIKit;
+namespace TableViewApp
+{
+    public class FruitsTableViewSource : UITableViewSource
+    {
+        private List<string> fruits;
+
+        public FruitsTableViewSource()
+        {
+        }
+
+        public FruitsTableViewSource(List<string> fruits)
+        {
+            this.fruits = fruits;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = new UITableViewCell(UITableViewCellStyle.Default, "");
+            cell.TextLabel.Text = fruits[indexPath.Row];
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return fruits.Count;
+        }
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            Console.WriteLine(fruits[indexPath.Row]);
+        }
+    }
+}
---
+++ TableViewApp/TableViewApp/ViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace TableViewApp
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            List<string> fruits = new List<string>{ "Apple","Mango","Pineapple","Banana","Kiwi"};
+            // Perform any additional setup after loading the view, typically from a nib.
+            FruitsTableView.Source = new FruitsTableViewSource(fruits);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+
+        }
+    }
+}
